7a22475e057436ee0143884e83a2eccc65b6ec92
tylertran349 <tylertran349@gmail.com>
2023-02-25 22:17:44 -0800

    Fix sign up and add bcrypt for creating/retrieving passwords securely

--- app.js
+++ app.js
@@ -25,61 +25,79 @@ app.set("views", __dirname);
 app.set("view engine", "ejs");
 
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+
+passport.use(
+    new LocalStrategy((username, password, done) => {
+      User.findOne({ username: username }, (err, user) => {
+        if (err) { 
+          return done(err);
+        }
+        if (!user) {
+          return done(null, false, { message: "Incorrect username" });
+        }
+        bcrypt.compare(password, user.password, (err, res) => {
+            if(res) {
+                // Passwords match, log user in
+                return done(null, user)
+            } else {
+                // Passwords do not match
+                return done(null, false, {message: "Incorrect password"});
+            }
+        })
+      });
+    })
+  );
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+User.findById(id, function(err, user) {
+    done(err, user);
+});
+});
+
 app.use(passport.initialize());
 app.use(passport.session());
-
+app.use(express.urlencoded({ extended: false }));
 app.use(function(req, res, next) {
-  res.locals.currentUser = req.user;
-  next();
+    res.locals.currentUser = req.user;
+    next();
 });
 
 app.get("/", (req, res) => {
-  res.render("index", { user: req.user });
-});
-
+    res.render("index", { user: req.user });
+  });
 app.get("/sign-up", (req, res) => res.render("sign-up-form"));
-
-app.post("/sign-up", (req, res, next) => {
-    const user = new User({
-      username: req.body.username,
-      password: req.body.password
-    }).save(err => {
-      if (err) { 
+app.get("/log-out", (req, res, next) => {
+    req.logout(function (err) {
+      if (err) {
         return next(err);
       }
       res.redirect("/");
     });
-});
-
-passport.use(new LocalStrategy((username, password, done) => {
-  User.findOne({username: username}, (err, user) => {
-    if(err) {
-      return done(err);
-    }
-    if(!user) {
-      return done(null, false, {message: "Incorrect username"});
-    }
-    if(user.password !== password) {
-      return done(null, false, {message: "Incorrect password"});
-    }
-    return done(null, user);
   });
-})
-);
 
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
+app.post("/sign-up", (req, res, next) => {
+    bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+        const user = new User({
+            username: req.body.username,
+            password: hashedPassword
+        }).save(err => {
+            if(err) {
+                return next(err);
+            }
+            res.redirect("/");
+        });
+    });
 });
 
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
-    done(err, user);
-  });
-});
-
-app.post("/log-in", passport.authenticate("local", {
-  successRedirect: "/",
-  failureRedirect: "/"
+app.post(
+"/log-in",
+passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/"
 })
 );
 
