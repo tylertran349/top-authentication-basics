0a669a4d4374ba9b21b42d97ee7c1b8ab9885607
tylertran349 <tylertran349@gmail.com>
2023-02-25 21:48:18 -0800

    ignore

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs");
 
 const mongoDb = "mongodb+srv://tylertran349:KleGWSEDlWryJjwD@cluster0.v1jifcx.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
@@ -26,6 +27,30 @@ app.set("view engine", "ejs");
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.render("index", { user: req.user });
+});
+
+app.get("/sign-up", (req, res) => res.render("sign-up-form"));
+
+app.post("/sign-up", (req, res, next) => {
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password
+    }).save(err => {
+      if (err) { 
+        return next(err);
+      }
+      res.redirect("/");
+    });
+});
+
 passport.use(new LocalStrategy((username, password, done) => {
   User.findOne({username: username}, (err, user) => {
     if(err) {
@@ -50,31 +75,6 @@ passport.deserializeUser(function(id, done) {
   User.findById(id, function(err, user) {
     done(err, user);
   });
-});
-
-app.get("/", (req, res) => {
-  res.render("index", { user: req.user });
-});
-app.get("/sign-up", (req, res) => res.render("sign-up-form"));
-app.get("/log-out", (req, res, next) => {
-  req.logout(function(err) {
-    if(err) {
-      return next(err);
-    }
-    res.redirect("/");
-  });
-});
-
-app.post("/sign-up", (req, res, next) => {
-    const user = new User({
-      username: req.body.username,
-      password: req.body.password
-    }).save(err => {
-      if (err) { 
-        return next(err);
-      }
-      res.redirect("/");
-    });
 });
 
 app.post("/log-in", passport.authenticate("local", {
